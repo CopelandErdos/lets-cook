f9ec29bd0e938cab8b24bc9cf4653fd5802036c6
sy <copeland_erdos@126.com>
2023-04-24 23:38:41 -0700

    3rd test - add quantity into ingredients hashmap

--- src/main/java/edu/berkeley/letscook/Dish.java
+++ src/main/java/edu/berkeley/letscook/Dish.java
@@ -1,23 +1,24 @@
 package edu.berkeley.letscook;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 // understands the ingredients of each dish
 public class Dish {
 
-    private final Set<Ingredient> ingredients;
+    private final Map<Ingredient, Double> ingredients;
 
     public Dish() {
-        this.ingredients = new HashSet<Ingredient>();
+        this.ingredients = new HashMap<Ingredient, Double>();
     }
 
-    public void addIngredient(Ingredient ingredient) {
-        this.ingredients.add(ingredient);
+    public void addIngredient(Ingredient ingredient, double quantity) {
+        this.ingredients.put(ingredient, quantity);
     }
 
-    public boolean canMake(Set<Ingredient> availableIngredients) {
-        for (Ingredient ingredient : ingredients) {
-            if (!availableIngredients.contains(ingredient)) {
+    public boolean canMake(Map<Ingredient, Double> availableIngredients) {
+        for (Ingredient ingredient : ingredients.keySet()) {
+            if (!(availableIngredients.keySet().contains(ingredient)
+                    && availableIngredients.get(ingredient) >= ingredients.get(ingredient))) {
                 return false;
             }
         }
--- src/test/java/edu/berkeley/letscook/DishTest.java
+++ src/test/java/edu/berkeley/letscook/DishTest.java
@@ -2,25 +2,37 @@ package edu.berkeley.letscook;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 
 public class DishTest {
 
-    static Set<Ingredient> availableIngredients = new HashSet<>();
+    static Map<Ingredient, Double> availableIngredients = new HashMap<>();
 
     static Ingredient fish = new Ingredient();
     static Ingredient potato = new Ingredient();
-
+    static Ingredient chicken = new Ingredient();
+    static Ingredient carrot = new Ingredient();
+    static Ingredient onion = new Ingredient();
     static Dish chips = new Dish();
     static Dish fishAndChips = new Dish();
+    static Dish curryChicken = new Dish();
 
 
     static {
-        availableIngredients.add(potato);
-        chips.addIngredient(potato);
-        fishAndChips.addIngredient(fish);
-        fishAndChips.addIngredient(potato);
+        availableIngredients.put(potato, 5.0);
+        availableIngredients.put(fish, 1.0);
+        availableIngredients.put(chicken, 2.5);
+        availableIngredients.put(carrot, 5.0);
+        availableIngredients.put(onion, 2.0);
+
+        chips.addIngredient(potato, 1.0);
+        fishAndChips.addIngredient(fish, 2.0);
+        fishAndChips.addIngredient(potato, 1.0);
+        curryChicken.addIngredient(chicken, 1.5);
+        curryChicken.addIngredient(potato, 1.0);
+        curryChicken.addIngredient(carrot, 0.5);
+        curryChicken.addIngredient(onion, 0.5);
     }
 
     @Test
@@ -32,4 +44,10 @@ public class DishTest {
     public void cannotMakeFishAndChips() {
         assertFalse(fishAndChips.canMake(availableIngredients));
     }
+
+    @Test
+    public void canMakeCurryChicken() {
+        assertTrue(curryChicken.canMake(availableIngredients));
+    }
+
 }
